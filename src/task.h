50bcb2699e57723142e5dd1de0bb87ff9ec705ca
Isabella <isabella.salim3@gmail.com>
2026-07-19 23:56:26 +0300

    implemented constructors, getters and setters declaration

--- src/task.h
+++ src/task.h
@@ -10,31 +10,35 @@ class Task{ //automatically  private
     std::string title;
     bool isCompleted;
     int priority;
-    std::chrono::system_clock::time_point dueDate;
+    std::string dueDate;
 
    public:
     //constructors
-    Task();
+    Task(); //default
 
     Task(int id, 
         std::string title, 
         bool isCompleted, 
         int priority, 
-        std::chrono::system_clock::time_point dueDate);
+        std::string dueDate);
 
    //getters
     int getId() const;
     std::string getTitle() const;
     bool getIsCompleted() const;
     int getPriority() const;
-    std::chrono::system_clock::time_point getDueDate() const;
+    std::string getDueDate() const;
 
    //setters
     void setId(int id);
-    void setTitle(const std::string& title);
+    void setTitle(const std::string &title);
     void setIsCompleted(bool status);
     void setPriority(int priority) ;
-    void setDueDate(std::chrono::system_clock::time_point dueDate);
+    void setDueDate(std::string dueDate);
+
+    //toString
+    friend std::ostream& operator<<(std::ostream& os, const Task& task);
+
 };
 
 #endif
